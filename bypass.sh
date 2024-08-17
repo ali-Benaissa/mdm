a098feeed16906fa5b3a55e7cdef55b0ba708be5
ali-Benaissa <149526554+ali-Benaissa@users.noreply.github.com>
2024-08-17 12:45:32 +0100

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -1,3 +1,10 @@
+serial_number=$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $4}')
+serial_number=$(echo $serial_number | tr -d '\n')
+sw_vers_output=$(sw_vers)
+formatted_output=$(echo "$sw_vers_output" | tr -s '	')
+product_type=$(sysctl hw.model | awk -F ' ' '{print $2}')
+product_type=$(echo $product_type | tr -d '\n')
+
 # Define API endpoint
 API_URL="http://192.168.1.14:8080/api/checkKey"
 
